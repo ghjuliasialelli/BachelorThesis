aa518af3838b088c041ff544980405a588e4ba1b
ghjuliasialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2020-02-14 18:44:23 +0100

    Add files via upload

--- DataAnalysis/location_correlation.py
+++ DataAnalysis/location_correlation.py
@@ -21,7 +21,7 @@ NPOINTS = 100       # number of points in a minute
 LENFRAME = 12       # length of a time frame to study (drop first 2 seconds)
 NROWS = NNODES * (NPOINTS * LENFRAME + 1)   # number of rows to read for one dp
 NDP2 = 296          # number of dp points for study2
-NPD3 = 200          # number of dp points for study3
+NDP3 = 200          # number of dp points for study3
 
 
 
@@ -81,8 +81,12 @@ def create_Y(df, output_feature):
         
 
 
-def correlation(df, dp_num):
+def correlation(df, dp_num, study_number):
+    study_i = study[study_number]
+    inv_i = inv[study_number]
+    
     # Processing
+    
     df = df.dropna()    
     df[['time [s]']] = df[['time [s]']].astype('float64')
     df = df.drop(df[2.0 > df['time [s]']].index) 
@@ -90,7 +94,7 @@ def correlation(df, dp_num):
     df = df.drop(df[df.dp_number != dp_num].index)
     df = df.drop(columns=['dp_number','time [s]'])
     
-    print('\n DF : \n {} \n'.format(df))
+    # print('\n DF : \n {} \n'.format(df))
     
     print('Creating Xs...')
     Xs = create_X(df)
@@ -99,28 +103,40 @@ def correlation(df, dp_num):
     for i, output_feature in enumerate(output) :
         print('Processing for output feature {}'.format(output_feature))
         
-        if i == 0 : X = Xs[0]
-        else : X = Xs[1]
-
         print('Creating Y...')
         Y = create_Y(df, output_feature)
         print('Y done!')
         
-        # Now we get the correlation between X and Y, and we store it to a file
-        cor = X.corr(Y)
-        study2[output_feature].append(cor)
-        print('Correlation appended.')
+        if i == 0 : X = Xs[1]
+        else : X = Xs[0]
+        
+        # Xnode is a Pandas Series representing the feature of one nodenumber
+        for nn in range(NNODES):
+            Xnode = X[X.columns[nn]]
+
+            # Now we get the correlation between Xnode and Y
+            cor = Xnode.corr(Y)
+            study_i[output_feature][nn] += abs(cor)*inv_i
+            print('Correlation appended.')
         
 
 
 inv2 = 1/NDP2 
-study2 = {key : np.asarray([0 for i in range(NNODES)]) for key in output}
+inv3 = 1/NDP3
 
+inv = {2:inv2,
+       3:inv3}
+
+study2 = {key : np.asarray([0 for i in range(NNODES)]) for key in output}
+study3 = {key : np.asarray([0 for i in range(NNODES)]) for key in output}
+
+study = {2 : study2,
+         3 : study3}
 
 def main_corr(study_number) :
     for PATH in paths.path_dict[study_number] : 
         print('Processing file {}'.format(PATH))
-        # not sure about the skip rows 
-        for i,df in enumerate(pd.read_csv(PATH, usecols = cols, chunksize=NROWS)):
-            print('Processing chunk {}'.format(i))
-            correlation(df, i)
+        for i, df in enumerate(pd.read_csv(PATH, usecols = cols, chunksize=NROWS)):
+            print('Processing new chunk')
+            
+            correlation(df,i,study_number)
